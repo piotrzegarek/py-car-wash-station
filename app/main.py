e387632312457361459789913ee00d7159bdfd35
Piotr Zegarek <pzegarek@intel471.com>
2024-10-19 17:18:23 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,52 @@
+from typing import List
+
+
 class Car:
-    # write your code here
-    pass
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int
+                 ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars_list: List[Car]) -> float:
+        """Washed list of carts and returns income."""
+        return sum(self.wash_single_car(car) for car in cars_list)
+
+    def rate_service(self, rating: int) -> None:
+        """Adds new rating and calculates new average and updates count."""
+        new_rates = (self.average_rating * self.count_of_ratings) + rating
+        self.count_of_ratings += 1
+        self.average_rating = round(new_rates / self.count_of_ratings, 1)
+
+    def wash_single_car(self, car: Car) -> float:
+        """Cleans car if wash power is greater than clean mark."""
+        price = 0
+        if self.clean_power > car.clean_mark:
+            price = self.calculate_washing_price(car)
+            car.clean_mark = self.clean_power
+        return price
+
+    def calculate_washing_price(self, car: Car) -> float:
+        """Calculates washing price of a car."""
+        return round(
+            (car.comfort_class
+             * (self.clean_power - car.clean_mark)
+             * self.average_rating
+             / self.distance_from_city_center),
+            1)
